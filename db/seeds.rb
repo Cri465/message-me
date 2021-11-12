612e8e4111137d0050193b21ee8b7ad9812e1ba4
Chris <chrism465@gmail.com>
2021-11-12 14:15:45 +0000

    added lots of things i lost track of time

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "shrek", password: "alsoshrek")
+User.create(username: "Mwozniak", password: "casserole")
+User.create(username: "bruce", password: "imbruce")
+User.create(username: "bonjovi", password: "hunter2")
+User.create(username: "daniel", password: "oleary")
+User.create(username: "george", password: "theboy")
